5344c37a6f0ed8433c9bd211bf9197140c8e5d37
M0H1K4N <nejer911@gmail.com>
2024-01-09 17:51:37 +0400

    Trying another way and learning RESTFULL API

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express');
 
 const app = express();
 
+// app.get('/main', (req, res) => {
+//     res.status(202).json({ message: 'hello from the serever side!', number: 2 });
+// });
+
+// app.post('/post', (req, res) => {
+//     res.send('u can post now here!');
+// });
+
+
+
 const port = 8080;
-app.get('/main', (req, res) => {
-  res.status(202).json({ message: 'hello from the serever side!', number: 2 });
-});
-
-app.post('/post', (req, res) => {
-  res.send('u can post now here!');
-});
-
 app.listen(port, () => {
   console.log(`listening to the port ${port}`);
 });
